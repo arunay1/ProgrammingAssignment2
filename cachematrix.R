21916d65894e8de8e5f8d58da906f4c9349da104
Arunay Kumar <arunay70@gmail.com>
2014-05-25 16:08:47 +0530

    My Submission to Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function takes an matrix as an argument and stores in in parent environment
+## set function stores the value of matrix in parent environment
+function(x = matrix()) {
+    m <- NULL
+    set <- function(y) 
+    {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function() m
+    list(set = set, get = get,setInverse = setInverse, getInverse = getInverse)
 }
 
-
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) 
+    {
+        message("getting cached Inverse Data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
 }
